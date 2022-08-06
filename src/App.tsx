21a6f9df4885e77b7b04c5f902c6a281487520f4
Dmytrii Furs <furs.dmitry@gmail.com>
2022-08-06 12:34:40 -0400

    BookCard- done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { HomePage } from './app/containers/HomePage';
+import tw from 'twin.macro';
+
+const AppContainer = styled.div`
+  ${ tw`
+  w-full
+  h-full
+  flex
+  flex-col
+  
+  `}
+`
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+   <AppContainer>
+      <HomePage/>
+   </AppContainer>
   );
 }
 
